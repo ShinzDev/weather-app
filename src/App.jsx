01e45814108e450d9918a989b69c8b64259d5f52
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2024-12-18 00:40:31 +0100

    a weather app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+import './App.css'
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-400 big-container">
+      <h1 className="text-4xl font-bold">Hello, Weather App!</h1>
+    </div>
+  );
+}
